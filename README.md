454fd8ed916825b3140ed1e29c584bcdbab32125
Constantino <constantinogola@gmail.com>
2024-08-28 13:32:08 +0100

    primeira versao

---
+++ README.md
@@ -0,0 +1,8 @@
+
+Nome: Gerador de Conselho
+
+=============================
+
+Para gerar um Conselho deves precionar o botão com a foto de um dado
+
+============================================================
---
+++ script.js
@@ -0,0 +1,20 @@
+const cod = document.getElementById('codigo')
+const conselho = document.getElementById("conselho")
+const botao = document.getElementById('botao')
+
+function buscarConselho(){
+    fetch("https://api.adviceslip.com/advice")
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        let advice = data.slip.advice
+        let id = data.slip.id
+        conselho.innerText = '"'+advice +'"'
+        cod.innerText = id
+    })
+}
+
+botao.onclick = () =>{
+    buscarConselho()
+}
